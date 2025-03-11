ae1b023001b55187e3f77ec0baddc714a0a4507d
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-03-11 14:49:12 +1300

    Added solution to 1480.

--- easy/1480.py
+++ easy/1480.py
@@ -6,24 +6,31 @@ Return the running sum of nums.
 
 from typing import List
 
-#region Solution
+# region Solution
 
 def running_sum(nums: List[int]) -> List[int]:
-    # TODO: Solution incoming...
-    return [0, 1, 2, 3, 4]
+    result: List[int] = []
+
+    for index, num in enumerate(nums):
+        if index == 0:
+            result.append(num)
+        else:
+            result.append(result[index - 1] + num)
+
+    return result
 
 
-#endregion
+# endregion
 
-#region Tests
+# region Tests
 
-# Expected: [1,3,6,10]
+# Expected: [1, 3, 6, 10]
 print(running_sum([1, 2, 3, 4]))
 
-# Expected: [1,2,3,4,5]
+# Expected: [1, 2, 3, 4, 5]
 print(running_sum([1, 1, 1, 1, 1]))
 
-# Expected: [3,4,6,16,17]
+# Expected: [3, 4, 6, 16, 17]
 print(running_sum([3, 1, 2, 10, 1]))
 
-#endregion
+# endregion
