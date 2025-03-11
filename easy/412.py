36aaeb61bd5ed4e4638310ce4c6f7c9f398d4042
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-03-11 16:13:49 +1300

    Added improvement to 412.

--- easy/412.py
+++ easy/412.py
@@ -14,14 +14,18 @@ def fizz_buzz(n: int) -> List[str]:
     results = []
 
     for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            results.append("FizzBuzz")
-        elif i % 3 == 0:
-            results.append("Fizz")
-        elif i % 5 == 0:
-            results.append("Buzz")
-        else:
-            results.append(str(i))
+        answer = ""
+
+        if i % 3 == 0:
+            answer += "Fizz"
+
+        if i % 5 == 0:
+            answer += "Buzz"
+
+        if answer == "":
+            answer = str(i)
+
+        results.append(answer)
 
     return results
 
