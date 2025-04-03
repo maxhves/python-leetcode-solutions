6159d5e3211e23d8eecae2d34466f24c866f5c2a
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-04-03 16:05:45 +1300

    Added TreeNode class.

---
+++ common/tree_node.py
@@ -0,0 +1,33 @@
+from collections import deque
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def build_tree(values: List[Optional[int]]) -> Optional[TreeNode]:
+    if not values:
+        return None
+
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+
+    while i < len(values):
+        node = queue.popleft()
+
+        if values[i] is not None:
+            node.left = TreeNode(values[i])
+            queue.append(node.left)
+        i += 1
+
+        if i < len(values) and values[i] is not None:
+            node.right = TreeNode(values[i])
+            queue.append(node.right)
+        i += 1
+
+    return root
